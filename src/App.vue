3041a5e9d5eb086fb3e965eaf17d7d5185f86bad
Rian Priansyah <rian.priansyah04@gmail.com>
2023-08-28 12:41:45 +0700

    menambahkan navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link :to="{ name: 'product' }">Product</router-link> | <router-link :to="{ name: 'productdetail', params: { id: 123 } }">Detail</router-link> |
-      <router-link :to="{ name: 'cart' }">Cart</router-link>
-    </nav>
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 * {
